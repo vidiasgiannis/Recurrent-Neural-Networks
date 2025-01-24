613c4b0239a2d65305f69d5693c12883196382f9
vidiasgiannis <vidiasgiannis@gmail.com>
2025-01-24 14:54:10 +0000

    Final Step In Question 1, check the runner.py method

--- cw1_code_data/nluplus_cw1/code/runner.py
+++ cw1_code_data/nluplus_cw1/code/runner.py
@@ -447,6 +447,10 @@ if __name__ == "__main__":
 
         ##########################
         # --- your code here --- #
+        rnn_model = RNN(vocab_size,hdim,vocab_size)
+        
+        run_loss = rnn_model.train_np(X_train, D_train, X_dev, D_dev, 
+                           back_steps=lookback, learning_rate=lr, epochs=1)
         ##########################
 
         run_loss = -1
